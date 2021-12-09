24101a373826bb852e131e0bce6b00f399c44126
Farjad Suhail <farjad.suhail@systemsltd.com>
2021-12-09 13:25:39 +0500

    work done without the service.ts file

---
+++ src/app/posts.service.ts
@@ -0,0 +1,15 @@
+//not doing anything in this branch
+
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class PostsService {
+
+    createAndStorePost(title: string, content: string) {
+        // ..
+    }
+
+    fetchPosts() {
+
+    }
+}
